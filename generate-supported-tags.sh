bb1688bb797b6a99188b4b27d3716fbd18b88834
Victor Popkov <victor@popkov.me>
2024-05-14 17:40:43 +0300

    Refactor generate-supported-tags.sh

--- generate-supported-tags.sh
+++ generate-supported-tags.sh
@@ -1,65 +1,50 @@
 #!/usr/bin/env bash
 
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
-COMMIT_ID=$(git rev-parse --verify HEAD)
+COMMIT_ID="$(git rev-parse --verify HEAD)"
 DISTS=('debian' 'alpine')
-URL='https://github.com/dstmodders/docker-dst-mod'
-VERSIONS=()
+JSON="$(cat ./versions.json)"
+REPOSITORY='https://github.com/dstmodders/docker-dst-mod'
+VERSIONS_KEYS=()
 
-mapfile -t VERSIONS < <(jq -r 'keys[]' ./versions.json)
-IFS=$'\n' VERSIONS=($(sort -rV <<< "${VERSIONS[*]}")); unset IFS
+mapfile -t VERSIONS_KEYS < <(jq -r 'keys[]' <<< "$JSON")
+# shellcheck disable=SC2207
+IFS=$'\n' VERSIONS_KEYS=($(sort -rV <<< "${VERSIONS_KEYS[*]}")); unset IFS
 
 readonly BASE_DIR
 readonly COMMIT_ID
 readonly DISTS
-readonly URL
-readonly VERSIONS
-
-# https://stackoverflow.com/a/17841619
-function join_by {
-  local d="${1-}"
-  local f="${2-}"
-  if shift 2; then
-    printf %s "$f" "${@/#/$d}";
-  fi
-}
-
-function jq_value {
-  local from="$1"
-  local key="$2"
-  local name="$3"
-  jq -r ".[${key}] | .${name}" "${from}"
-}
+readonly JSON
+readonly REPOSITORY
+readonly VERSIONS_KEYS
 
 function print_url() {
   local tags="$1"
   local commit="$2"
-  local dist="$3"
-  local url="[$tags](${URL}/blob/${commit}/${dist}/Dockerfile)"
-  echo "- ${url}"
+  local directory="$3"
+  local url="[$tags]($REPOSITORY/blob/$commit/$directory/Dockerfile)"
+  echo "- $url"
 }
 
-cd "${BASE_DIR}" || exit 1
+cd "$BASE_DIR" || exit 1
 
 printf "## Supported tags and respective \`Dockerfile\` links\n\n"
 
-for v in "${VERSIONS[@]}"; do
+for key in "${VERSIONS_KEYS[@]}"; do
   for dist in "${DISTS[@]}"; do
-    commit="${COMMIT_ID}"
-    latest=$(jq_value ./versions.json "${v}" 'latest')
+    latest=$(jq -r ".[$key] | .latest" <<< "$JSON")
 
-    tag_dist="${dist}"
+    tag_dist="$dist"
 
-    tags=''
-    if [ "${dist}" == 'debian' ]; then
-      tags="\`${tag_dist}\`"
-      if [ "${latest}" == 'true' ]; then
-        tags="${tags}, \`latest\`"
-      fi
-    else
-      tags="\`${tag_dist}\`"
-    fi
+    tags="\`$tag_dist\`"
+    case "$dist" in
+      debian)
+        if [ "$latest" == 'true' ]; then
+          tags="$tags, \`latest\`"
+        fi
+      ;;
+    esac
 
-    print_url "${tags}" "${commit}" "${dist}"
+    print_url "$tags" "$COMMIT_ID" "$dist"
   done
 done
